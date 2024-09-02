7cebe24cc6c88a7ca628901e7889edd187bba095
Kenny Luu <kennyluu74@gmail.com>
2024-09-02 12:46:05 -0700

    Add lsp support

--- init.lua
+++ init.lua
@@ -1,9 +1,24 @@
 require("config.lazy")
 -- VIM Settings
-
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set nu")
 vim.cmd("set ai")
+
+-- vim.cmd("inoremap ( ()<Esc>i")
+-- vim.cmd("inoremap { {}<Esc>i")
+-- vim.cmd("inoremap {<CR> {<CR>}<Esc>O")
+-- vim.cmd("inoremap [ []<Esc>i")
+-- vim.cmd("inoremap < <><Esc>i")
+-- vim.cmd("inoremap \' \'\'<Esc>i")
+-- vim.cmd("inoremap \" \"\"<Esc>i")
+
+vim.cmd("inoremap { {}<Esc>ha")
+vim.cmd("inoremap ( ()<Esc>ha")
+vim.cmd("inoremap [ []<Esc>ha")
+vim.cmd("inoremap \" \"\"<Esc>ha")
+vim.cmd("inoremap \' \'\'<Esc>ha")
+vim.cmd("inoremap ` ``<Esc>ha")
+
