877028efc2a67df90bfd4ac900677ce38c6b5b5a
Kenny Luu <kenny.luu@crowdstrike.com>
2025-08-05 16:49:43 -0700

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,14 +23,13 @@ vim.g.maplocalleader = " "
 
 -- Setup lazy.nvim
 require("lazy").setup({
-    
     spec = {
-        { import="plugins" }, 
+        { import="plugins" },
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "catppuccin" } },
     -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = { enabled = false },
 })
 
